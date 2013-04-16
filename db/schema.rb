261474996fd06327d349f3e4ef8e0b0aee049a52
Muhammad <majidnaviwala@gmail.com>
2013-04-15 21:45:25 -0500

    workspace.xml modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130414002130) do
+ActiveRecord::Schema.define(:version => 20130415155200) do
 
   create_table "artists", :force => true do |t|
     t.string   "lastName"
@@ -57,6 +57,12 @@ ActiveRecord::Schema.define(:version => 20130414002130) do
     t.string   "email"
     t.string   "password"
     t.integer  "role"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "messages", :force => true do |t|
+    t.text     "content"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
